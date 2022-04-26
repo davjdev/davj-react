6a62c6071a392f39cd132fffc4f60337e35fb409
d <d@ds-MacBook-Pro.attlocal.net>
2022-04-26 02:33:43 -0500

    Completed initial update

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+# deploy.sh
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+# echo Linting..
+# npm run lint
+echo Building. this may take a minute...
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'davj.dev' > CNAME
+
+echo Deploying..
+git init
+git add -A && git commit -m "deploy"
+
+# deploy
+git push -f git@github.com:davjdev/davj-react.git master:gh-pages
+
+cd -
