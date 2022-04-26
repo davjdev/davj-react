4fe2c01b40a73e532f878e6363231b0cfa236839
d <d@ds-MacBook-Pro.attlocal.net>
2022-04-26 01:51:42 -0500

    Completed initial update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,24 @@
 import './Home.css';
+import { BsLinkedin, BsTwitter } from "react-icons/bs";
+import { RiShip2Fill } from "react-icons/ri";
 
 function Home() {
   return (
-    <div className="Home">
-      hello
-      <br/>
-      this
-      <br/>
-      is
-      <br/>
-      home
+    <div class="home">
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
+      <div class="flex-home-main-container">
+        <div class="home-main-container" id="">
+          <h1 class="home-main-header">Hello, I'm David!</h1>
+          <p class="home-main-intro">
+            I am a Software Engineer who likes to solve problems, <RiShip2Fill className="brand-icon-small" /> products and fail fast.
+          </p>
+          <a href="https://twitter.com/davjdev/"><BsTwitter className="brand-icon" /></a>
+          <a href="https://www.linkedin.com/in/davjdev/"><BsLinkedin className="brand-icon" /></a>
+        </div>
+        <div class="footer">
+          © davj.dev
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/topnav/TopNav.js
+++ src/components/topnav/TopNav.js
@@ -7,18 +7,17 @@ function TopNav() {
   return (
     <div className="top-nav">
       <Navbar className="top-navbar-main" bg="light" expand="lg">
-        {/* <div class="mr-auto top-navbar-brand-container"> */}
-          <Navbar.Brand className="top-navbar-brand" href="#">
-            <img
-              src={logo}
-              width="40"
-              height="40"
-            />
-          </Navbar.Brand>
-          <div className="top-navbar-link-container">
-            <Nav.Link className="top-navbar-link" href="#link">About</Nav.Link>
-            <Nav.Link className="top-navbar-link" href="#link">Blog</Nav.Link>
-          </div>
+        <Navbar.Brand className="top-navbar-brand" href="#">
+          <img
+            src={logo}
+            width="40"
+            height="40"
+          />
+        </Navbar.Brand>
+        <div className="top-navbar-link-container">
+          {/* <Nav.Link className="top-navbar-link" href="#">About</Nav.Link> */}
+          {/* <Nav.Link className="top-navbar-link" href="#">Blog</Nav.Link> */}
+        </div>
       </Navbar>
     </div>
   );
